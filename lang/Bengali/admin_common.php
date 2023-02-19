8e361eaf29b8456e8ae254b795eb837dff103ae4
Sakhawat Hossain <sakhwt.hssain@gmail.com>
2023-02-19 17:14:01 +0600

    Add files via upload

---
+++ lang/Bengali/admin_common.php
@@ -0,0 +1,44 @@
+<?php
+
+// Language definitions used in admin_common.php
+$lang_admin_common = array(
+
+// The menu
+'Admin menu'			=>	'এডমিন  মেন্যু',
+'Plugins menu'			=>	'প্লাগিন্স মেন্যু',
+'Moderator menu'		=>	'মডারেটর  মেন্যু',
+'Index'					=>	'ইনডেক্স',
+'Categories'			=>	'ক্যাটাগরি গুলি',
+'Forums'				=>	'ফোরাম গুলি',
+'Users'					=>	'ব্যাবহারকারি গণ',
+'User groups'			=>	'ব্যাবহারকারি গ্রুপ গুলি',
+'Options'				=>	'অপশন গুলি',
+'Permissions'			=>	'অনুমতি গুলি',
+'Censoring'				=>	'সেন্সর করা',
+'Bans'					=>	'ব্যানগুলি',
+'Prune'					=>	'ছাঁটাই',
+'Maintenance'			=>	'রক্ষণাবেক্ষণ',
+'Reports'				=>	'রিপোর্ট গুলি',
+'Server statistics'				=>	'সার্ভার পরিসংখ্যান',
+
+'Admin'					=>	'এডমিন',
+'Go back'				=>	'ফিরে যান',
+'Delete'				=>	'মুছুন',
+'Update'				=>	'হালনাগাদ',
+'Add'					=>	'যুক্ত করা',
+'Edit'					=>	'সম্পাদনা',
+'Remove'				=>	'অপসারণ',
+'Yes'					=>	'হ্যাঁ',
+'No'					=>	'না',
+'Save changes'			=>	'পরিবর্তনগুলোর সংরক্ষন',
+'Save'					=>	'সংরক্ষন',
+'here'					=>	'এখানে',
+'Action'				=>	'একশন',
+'None'					=>	'কোনোটিই নয়',
+'Maintenance mode'		=>	'রক্ষণাবেক্ষণ  মোড', // Used for link text in more than one file
+
+// Admin loader
+'No plugin message'		=>	'প্লাগইন ডিরেক্টরিতে  %s নামে কোনও প্লাগইন নেই।',
+'Plugin failed message'	=>	'প্লাগইনের লোড - <strong>%s</strong> - ব্যর্থ হয়েছে.',
+
+);
